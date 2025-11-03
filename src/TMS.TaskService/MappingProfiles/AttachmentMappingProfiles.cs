3d2579a08f246bf2d60435f66e1ea61febf09af4
Alexander Yurtaev <alexander.yurtaev@gmail.com>
2025-11-03 17:12:31 +0200

    Implemet MappingProfiles for Comments and Attachments.

---
+++ src/TMS.TaskService/MappingProfiles/AttachmentMappingProfiles.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using TMS.TaskService.Entities;
+using TMS.TaskService.Models.Attachments;
+
+namespace TMS.TaskService.MappingProfiles;
+
+/// <summary>
+/// 
+/// </summary>
+public class AttachmentMappingProfiles : Profile
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public AttachmentMappingProfiles()
+    {
+        #region AttachmentCreate
+
+        CreateMap<AttachmentCreate, AttachmentEntity>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+        CreateMap<AttachmentEntity, AttachmentCreate>();
+
+        #endregion AttachmentCreate
+    }
+}
---
+++ src/TMS.TaskService/MappingProfiles/CommentMappingProfiles.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using TMS.TaskService.Entities;
+using TMS.TaskService.Models.Comments;
+
+namespace TMS.TaskService.MappingProfiles;
+
+/// <summary>
+/// 
+/// </summary>
+public class CommentMappingProfiles : Profile
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public CommentMappingProfiles()
+    {
+        #region CommentCreate
+
+        CreateMap<CommentCreate, CommentEntity>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+        CreateMap<CommentEntity, CommentCreate>();
+
+        #endregion CommentCreate
+
+        #region CommentUpdate
+
+        CreateMap<CommentUpdate, CommentEntity>();
+
+        CreateMap<CommentEntity, CommentUpdate>();
+
+        #endregion CommentUpdate
+    }
+}
