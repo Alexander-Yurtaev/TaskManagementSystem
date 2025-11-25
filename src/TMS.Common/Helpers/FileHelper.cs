39c771ce3820732e806bb93bc06ae909707f6ede
Alexander Yurtaev <alexander.yurtaev@gmail.com>
2025-11-26 01:26:18 +0300

    Fix IsPathSafe.

--- src/TMS.Common/Helpers/FileHelper.cs
+++ src/TMS.Common/Helpers/FileHelper.cs
@@ -4,8 +4,12 @@ public static class FileHelper
 {
     public static bool IsPathSafe(string basePath, string userPath)
     {
+        ArgumentNullException.ThrowIfNull(basePath);
+
         var fullBasePath = Path.GetFullPath(basePath).TrimEnd(Path.DirectorySeparatorChar);
         var fullPath = Path.GetFullPath(Path.Combine(basePath, userPath));
-        return fullPath.StartsWith(fullBasePath + Path.DirectorySeparatorChar);
+
+        return fullPath.StartsWith(fullBasePath + Path.DirectorySeparatorChar) ||
+               fullPath == fullBasePath;
     }
 }
