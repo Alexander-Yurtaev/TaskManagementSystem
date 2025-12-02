f4af7d574f138c810bc242051afafad4a9c7512f
Alexander Yurtaev <alexander.yurtaev@gmail.com>
2025-12-02 10:17:45 +0300

    Remove /setup method.

--- src/TMS.AuthService/Extensions/ApiEndpoints/MigrationEndpoints.cs
+++ src/TMS.AuthService/Extensions/ApiEndpoints/MigrationEndpoints.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using TMS.AuthService.Data;
 using TMS.Common.Helpers;
@@ -23,38 +22,18 @@ public static class MigrationEndpoints
     /// <response code="500">Ошибка при миграции.</response>
     public static RouteHandlerBuilder AddMigrateEndpoint(this IEndpointRouteBuilder endpoints)
     {
-        endpoints.MapGet("/setup", async (
-                HttpContext context,
-                [FromServices] AuthDataContext db,
-                [FromServices] ILogger<IApplicationBuilder> logger) =>
-            {
-                if (await DatabaseIsExists(db))
-                {
-                    return Results.BadRequest("Система уже настроена.");
-                }
-
-                return await Migrate(db, logger);
-            })
-            .WithName("SetupDatabase")
-            .AllowAnonymous()
-            .WithMetadata(new OpenApiOperation
-            {
-                Summary = "Запуск начальной настройки БД."
-            })
-            .Produces<string>(StatusCodes.Status400BadRequest)
-            .Produces(StatusCodes.Status500InternalServerError)
-            .WithOpenApi(operation =>
-            {
-                operation = OpenApiMigrationHelper.InitOperationForSetup(operation, "tms-auth-db", "Auth.Setup");
-                return operation;
-            });
-
         return endpoints.MapGet("/migrate", async (
                 HttpContext context,
                 [FromServices] AuthDataContext db,
                 [FromServices] ILogger<IApplicationBuilder> logger) =>
         {
-            return await Migrate(db, logger);
+            var details = await MigrateHelper.Migrate(db, logger);
+            if (details.StatusCode == StatusCodes.Status200OK)
+            {
+                return Results.Ok(details.Result);
+            }
+
+            return Results.Problem(detail: details.Detail, statusCode: details.StatusCode);
         })
             .WithName("MigrateDatabase")
             .RequireAuthorization()
@@ -71,32 +50,4 @@ public static class MigrationEndpoints
                 return operation;
             });
     }
-
-    #region Private Methods
-
-    private static async Task<bool> DatabaseIsExists(DbContext db)
-    {
-        try
-        {
-            var am = await db.Database.GetAppliedMigrationsAsync();
-            return am.Any();
-        }
-        catch (Exception)
-        {
-            return false;
-        }
-    }
-
-    private static async Task<IResult> Migrate(AuthDataContext db, ILogger<IApplicationBuilder> logger)
-    {
-        var details = await MigrateHelper.Migrate(db, logger);
-        if (details.StatusCode == StatusCodes.Status200OK)
-        {
-            return Results.Ok(details.Result);
-        }
-
-        return Results.Problem(detail: details.Detail, statusCode: details.StatusCode);
-    }
-
-    #endregion Private Methods
 }
